e386bc5f9d3fa9a75d558516df23638ebeecdb26
森井ゴンザレス <morygonzalez@gmail.com>
2015-04-09 21:05:23 +0900

    configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'exception_notification/slacky'
+require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.before do
+    WebMock.disable_net_connect!
+  end
+end
